0c50d1f04820fd5d7a09ddd8e77ca42c7424f3f6
tr0b <trobin3000600@gmail.com>
2020-08-21 20:51:40 -0600

    Final Project DBA -Added separate file for read_write permissions for admin role

---
+++ grant_read_write_administrator.sql
@@ -0,0 +1,669 @@
+/*Grant read-write on views for admin*/
+GRANT DELETE ON v_Tipo_Persona TO Rol_Administrativo
+GO
+GRANT DELETE ON v_Formacion_Educativa TO Rol_Administrativo
+GO
+
+GRANT DELETE ON v_Tipo_Mecanismo TO Rol_Administrativo
+GO
+
+GRANT DELETE ON v_Categoria TO Rol_Administrativo
+GO
+
+GRANT DELETE ON v_Tipo_Identificacion TO Rol_Administrativo
+GO
+
+GRANT DELETE ON v_Identificacion TO Rol_Administrativo
+GO
+
+GRANT DELETE ON v_Persona TO Rol_Administrativo
+GO
+
+GRANT DELETE ON v_Tipo_Estado_Persona TO Rol_Administrativo
+GO
+
+GRANT DELETE ON v_Persona_Tipo TO Rol_Administrativo
+GO
+
+GRANT DELETE ON v_Estado_Persona TO Rol_Administrativo 
+GO
+
+GRANT DELETE ON v_Mecanismo_Contacto TO Rol_Administrativo
+GO
+
+GRANT DELETE ON v_Moneda TO Rol_Administrativo
+GO
+
+GRANT DELETE ON v_Agencia TO Rol_Administrativo
+GO
+
+GRANT DELETE ON v_Tipo_Geografia TO Rol_Administrativo
+GO
+
+GRANT DELETE ON v_Tipo_Ingreso TO Rol_Administrativo
+GO
+
+GRANT DELETE ON v_Ingreso TO Rol_Administrativo
+GO
+
+GRANT DELETE ON v_Tipo_Direccion TO Rol_Administrativo
+GO
+
+GRANT DELETE ON v_Direccion TO Rol_Administrativo
+GO
+
+GRANT DELETE ON v_Geografia TO Rol_Administrativo
+GO
+
+GRANT DELETE ON v_Tipo_Relacion TO Rol_Administrativo
+GO
+
+GRANT DELETE ON v_Relacion TO Rol_Administrativo
+GO
+
+GRANT DELETE ON v_Sitio_Web TO Rol_Administrativo
+GO
+
+GRANT DELETE ON v_Naturaleza_Industria TO Rol_Administrativo 
+GO
+
+GRANT DELETE ON v_Industria_Persona TO Rol_Administrativo
+GO
+
+GRANT DELETE ON v_Garantia TO Rol_Administrativo
+GO
+
+GRANT DELETE ON v_Tipo_Operacion_Crediticia TO Rol_Administrativo
+GO
+
+GRANT DELETE ON v_Estado_Operacion_Crediticia TO Rol_Administrativo
+GO
+
+GRANT DELETE ON v_Estado_Movimiento TO Rol_Administrativo
+GO
+
+GRANT DELETE ON v_Tipo_Movimiento TO Rol_Administrativo
+GO
+
+GRANT DELETE ON v_Operacion_Crediticia TO Rol_Administrativo
+GO
+
+GRANT DELETE ON v_Movimiento TO Rol_Administrativo
+GO
+
+GRANT DELETE ON v_Garantia_Operacion_Crediticia TO Rol_Administrativo
+GO
+/**Grant read write permissions on tables for admin**/
+
+GRANT DELETE ON Tipo_Persona TO Rol_Administrativo
+GO
+GRANT DELETE ON Formacion_Educativa TO Rol_Administrativo
+GO
+
+GRANT DELETE ON Tipo_Mecanismo TO Rol_Administrativo
+GO
+
+GRANT DELETE ON Categoria TO Rol_Administrativo
+GO
+
+GRANT DELETE ON Tipo_Identificacion TO Rol_Administrativo
+GO
+
+GRANT DELETE ON Identificacion TO Rol_Administrativo
+GO
+
+GRANT DELETE ON Persona TO Rol_Administrativo
+GO
+
+GRANT DELETE ON Tipo_Estado_Persona TO Rol_Administrativo
+GO
+
+GRANT DELETE ON Persona_Tipo TO Rol_Administrativo
+GO
+
+GRANT DELETE ON Estado_Persona TO Rol_Administrativo 
+GO
+
+GRANT DELETE ON Mecanismo_Contacto TO Rol_Administrativo
+GO
+
+GRANT DELETE ON Moneda TO Rol_Administrativo
+GO
+
+GRANT DELETE ON Agencia TO Rol_Administrativo
+GO
+
+GRANT DELETE ON Tipo_Geografia TO Rol_Administrativo
+GO
+
+GRANT DELETE ON Tipo_Ingreso TO Rol_Administrativo
+GO
+
+GRANT DELETE ON Ingreso TO Rol_Administrativo
+GO
+
+GRANT DELETE ON Tipo_Direccion TO Rol_Administrativo
+GO
+
+GRANT DELETE ON Direccion TO Rol_Administrativo
+GO
+
+GRANT DELETE ON Geografia TO Rol_Administrativo
+GO
+
+GRANT DELETE ON Tipo_Relacion TO Rol_Administrativo
+GO
+
+GRANT DELETE ON Relacion TO Rol_Administrativo
+GO
+
+GRANT DELETE ON Sitio_Web TO Rol_Administrativo
+GO
+
+GRANT DELETE ON Naturaleza_Industria TO Rol_Administrativo 
+GO
+
+GRANT DELETE ON Industria_Persona TO Rol_Administrativo
+GO
+
+GRANT DELETE ON Garantia TO Rol_Administrativo
+GO
+
+GRANT DELETE ON Tipo_Operacion_Crediticia TO Rol_Administrativo
+GO
+
+GRANT DELETE ON Estado_Operacion_Crediticia TO Rol_Administrativo
+GO
+
+GRANT DELETE ON Estado_Movimiento TO Rol_Administrativo
+GO
+
+GRANT DELETE ON Tipo_Movimiento TO Rol_Administrativo
+GO
+
+GRANT DELETE ON Operacion_Crediticia TO Rol_Administrativo
+GO
+
+GRANT DELETE ON Movimiento TO Rol_Administrativo
+GO
+
+GRANT DELETE ON Garantia_Operacion_Crediticia TO Rol_Administrativo
+GO
+/*Updates*/
+GRANT UPDATE ON v_Tipo_Persona TO Rol_Administrativo
+GO
+GRANT UPDATE ON v_Formacion_Educativa TO Rol_Administrativo
+GO
+
+GRANT UPDATE ON v_Tipo_Mecanismo TO Rol_Administrativo
+GO
+
+GRANT UPDATE ON v_Categoria TO Rol_Administrativo
+GO
+
+GRANT UPDATE ON v_Tipo_Identificacion TO Rol_Administrativo
+GO
+
+GRANT UPDATE ON v_Identificacion TO Rol_Administrativo
+GO
+
+GRANT UPDATE ON v_Persona TO Rol_Administrativo
+GO
+
+GRANT UPDATE ON v_Tipo_Estado_Persona TO Rol_Administrativo
+GO
+
+GRANT UPDATE ON v_Persona_Tipo TO Rol_Administrativo
+GO
+
+GRANT UPDATE ON v_Estado_Persona TO Rol_Administrativo 
+GO
+
+GRANT UPDATE ON v_Mecanismo_Contacto TO Rol_Administrativo
+GO
+
+GRANT UPDATE ON v_Moneda TO Rol_Administrativo
+GO
+
+GRANT UPDATE ON v_Agencia TO Rol_Administrativo
+GO
+
+GRANT UPDATE ON v_Tipo_Geografia TO Rol_Administrativo
+GO
+
+GRANT UPDATE ON v_Tipo_Ingreso TO Rol_Administrativo
+GO
+
+GRANT UPDATE ON v_Ingreso TO Rol_Administrativo
+GO
+
+GRANT UPDATE ON v_Tipo_Direccion TO Rol_Administrativo
+GO
+
+GRANT UPDATE ON v_Direccion TO Rol_Administrativo
+GO
+
+GRANT UPDATE ON v_Geografia TO Rol_Administrativo
+GO
+
+GRANT UPDATE ON v_Tipo_Relacion TO Rol_Administrativo
+GO
+
+GRANT UPDATE ON v_Relacion TO Rol_Administrativo
+GO
+
+GRANT UPDATE ON v_Sitio_Web TO Rol_Administrativo
+GO
+
+GRANT UPDATE ON v_Naturaleza_Industria TO Rol_Administrativo 
+GO
+
+GRANT UPDATE ON v_Industria_Persona TO Rol_Administrativo
+GO
+
+GRANT UPDATE ON v_Garantia TO Rol_Administrativo
+GO
+
+GRANT UPDATE ON v_Tipo_Operacion_Crediticia TO Rol_Administrativo
+GO
+
+GRANT UPDATE ON v_Estado_Operacion_Crediticia TO Rol_Administrativo
+GO
+
+GRANT UPDATE ON v_Estado_Movimiento TO Rol_Administrativo
+GO
+
+GRANT UPDATE ON v_Tipo_Movimiento TO Rol_Administrativo
+GO
+
+GRANT UPDATE ON v_Operacion_Crediticia TO Rol_Administrativo
+GO
+
+GRANT UPDATE ON v_Movimiento TO Rol_Administrativo
+GO
+
+GRANT UPDATE ON v_Garantia_Operacion_Crediticia TO Rol_Administrativo
+GO
+/**Grant read write permissions on tables for admin**/
+
+GRANT UPDATE ON Tipo_Persona TO Rol_Administrativo
+GO
+GRANT UPDATE ON Formacion_Educativa TO Rol_Administrativo
+GO
+
+GRANT UPDATE ON Tipo_Mecanismo TO Rol_Administrativo
+GO
+
+GRANT UPDATE ON Categoria TO Rol_Administrativo
+GO
+
+GRANT UPDATE ON Tipo_Identificacion TO Rol_Administrativo
+GO
+
+GRANT UPDATE ON Identificacion TO Rol_Administrativo
+GO
+
+GRANT UPDATE ON Persona TO Rol_Administrativo
+GO
+
+GRANT UPDATE ON Tipo_Estado_Persona TO Rol_Administrativo
+GO
+
+GRANT UPDATE ON Persona_Tipo TO Rol_Administrativo
+GO
+
+GRANT UPDATE ON Estado_Persona TO Rol_Administrativo 
+GO
+
+GRANT UPDATE ON Mecanismo_Contacto TO Rol_Administrativo
+GO
+
+GRANT UPDATE ON Moneda TO Rol_Administrativo
+GO
+
+GRANT UPDATE ON Agencia TO Rol_Administrativo
+GO
+
+GRANT UPDATE ON Tipo_Geografia TO Rol_Administrativo
+GO
+
+GRANT UPDATE ON Tipo_Ingreso TO Rol_Administrativo
+GO
+
+GRANT UPDATE ON Ingreso TO Rol_Administrativo
+GO
+
+GRANT UPDATE ON Tipo_Direccion TO Rol_Administrativo
+GO
+
+GRANT UPDATE ON Direccion TO Rol_Administrativo
+GO
+
+GRANT UPDATE ON Geografia TO Rol_Administrativo
+GO
+
+GRANT UPDATE ON Tipo_Relacion TO Rol_Administrativo
+GO
+
+GRANT UPDATE ON Relacion TO Rol_Administrativo
+GO
+
+GRANT UPDATE ON Sitio_Web TO Rol_Administrativo
+GO
+
+GRANT UPDATE ON Naturaleza_Industria TO Rol_Administrativo 
+GO
+
+GRANT UPDATE ON Industria_Persona TO Rol_Administrativo
+GO
+
+GRANT UPDATE ON Garantia TO Rol_Administrativo
+GO
+
+GRANT UPDATE ON Tipo_Operacion_Crediticia TO Rol_Administrativo
+GO
+
+GRANT UPDATE ON Estado_Operacion_Crediticia TO Rol_Administrativo
+GO
+
+GRANT UPDATE ON Estado_Movimiento TO Rol_Administrativo
+GO
+
+GRANT UPDATE ON Tipo_Movimiento TO Rol_Administrativo
+GO
+
+GRANT UPDATE ON Operacion_Crediticia TO Rol_Administrativo
+GO
+
+GRANT UPDATE ON Movimiento TO Rol_Administrativo
+GO
+
+GRANT UPDATE ON Garantia_Operacion_Crediticia TO Rol_Administrativo
+GO
+/*Selects*/
+GRANT SELECT ON v_Tipo_Persona TO Rol_Administrativo
+GO
+GRANT SELECT ON v_Formacion_Educativa TO Rol_Administrativo
+GO
+
+GRANT SELECT ON v_Tipo_Mecanismo TO Rol_Administrativo
+GO
+
+GRANT SELECT ON v_Categoria TO Rol_Administrativo
+GO
+
+GRANT SELECT ON v_Tipo_Identificacion TO Rol_Administrativo
+GO
+
+GRANT SELECT ON v_Identificacion TO Rol_Administrativo
+GO
+
+GRANT SELECT ON v_Persona TO Rol_Administrativo
+GO
+
+GRANT SELECT ON v_Tipo_Estado_Persona TO Rol_Administrativo
+GO
+
+GRANT SELECT ON v_Persona_Tipo TO Rol_Administrativo
+GO
+
+GRANT SELECT ON v_Estado_Persona TO Rol_Administrativo 
+GO
+
+GRANT SELECT ON v_Mecanismo_Contacto TO Rol_Administrativo
+GO
+
+GRANT SELECT ON v_Moneda TO Rol_Administrativo
+GO
+
+GRANT SELECT ON v_Agencia TO Rol_Administrativo
+GO
+
+GRANT SELECT ON v_Tipo_Geografia TO Rol_Administrativo
+GO
+
+GRANT SELECT ON v_Tipo_Ingreso TO Rol_Administrativo
+GO
+
+GRANT SELECT ON v_Ingreso TO Rol_Administrativo
+GO
+
+GRANT SELECT ON v_Tipo_Direccion TO Rol_Administrativo
+GO
+
+GRANT SELECT ON v_Direccion TO Rol_Administrativo
+GO
+
+GRANT SELECT ON v_Geografia TO Rol_Administrativo
+GO
+
+GRANT SELECT ON v_Tipo_Relacion TO Rol_Administrativo
+GO
+
+GRANT SELECT ON v_Relacion TO Rol_Administrativo
+GO
+
+GRANT SELECT ON v_Sitio_Web TO Rol_Administrativo
+GO
+
+GRANT SELECT ON v_Naturaleza_Industria TO Rol_Administrativo 
+GO
+
+GRANT SELECT ON v_Industria_Persona TO Rol_Administrativo
+GO
+
+GRANT SELECT ON v_Garantia TO Rol_Administrativo
+GO
+
+GRANT SELECT ON v_Tipo_Operacion_Crediticia TO Rol_Administrativo
+GO
+
+GRANT SELECT ON v_Estado_Operacion_Crediticia TO Rol_Administrativo
+GO
+
+GRANT SELECT ON v_Estado_Movimiento TO Rol_Administrativo
+GO
+
+GRANT SELECT ON v_Tipo_Movimiento TO Rol_Administrativo
+GO
+
+GRANT SELECT ON v_Operacion_Crediticia TO Rol_Administrativo
+GO
+
+GRANT SELECT ON v_Movimiento TO Rol_Administrativo
+GO
+
+GRANT SELECT ON v_Garantia_Operacion_Crediticia TO Rol_Administrativo
+GO
+/**Grant read write permissions on tables for admin**/
+
+GRANT SELECT ON Tipo_Persona TO Rol_Administrativo
+GO
+GRANT SELECT ON Formacion_Educativa TO Rol_Administrativo
+GO
+
+GRANT SELECT ON Tipo_Mecanismo TO Rol_Administrativo
+GO
+
+GRANT SELECT ON Categoria TO Rol_Administrativo
+GO
+
+GRANT SELECT ON Tipo_Identificacion TO Rol_Administrativo
+GO
+
+GRANT SELECT ON Identificacion TO Rol_Administrativo
+GO
+
+GRANT SELECT ON Persona TO Rol_Administrativo
+GO
+
+GRANT SELECT ON Tipo_Estado_Persona TO Rol_Administrativo
+GO
+
+GRANT SELECT ON Persona_Tipo TO Rol_Administrativo
+GO
+
+GRANT SELECT ON Estado_Persona TO Rol_Administrativo 
+GO
+
+GRANT SELECT ON Mecanismo_Contacto TO Rol_Administrativo
+GO
+
+GRANT SELECT ON Moneda TO Rol_Administrativo
+GO
+
+GRANT SELECT ON Agencia TO Rol_Administrativo
+GO
+
+GRANT SELECT ON Tipo_Geografia TO Rol_Administrativo
+GO
+
+GRANT SELECT ON Tipo_Ingreso TO Rol_Administrativo
+GO
+
+GRANT SELECT ON Ingreso TO Rol_Administrativo
+GO
+
+GRANT SELECT ON Tipo_Direccion TO Rol_Administrativo
+GO
+
+GRANT SELECT ON Direccion TO Rol_Administrativo
+GO
+
+GRANT SELECT ON Geografia TO Rol_Administrativo
+GO
+
+GRANT SELECT ON Tipo_Relacion TO Rol_Administrativo
+GO
+
+GRANT SELECT ON Relacion TO Rol_Administrativo
+GO
+
+GRANT SELECT ON Sitio_Web TO Rol_Administrativo
+GO
+
+GRANT SELECT ON Naturaleza_Industria TO Rol_Administrativo 
+GO
+
+GRANT SELECT ON Industria_Persona TO Rol_Administrativo
+GO
+
+GRANT SELECT ON Garantia TO Rol_Administrativo
+GO
+
+GRANT SELECT ON Tipo_Operacion_Crediticia TO Rol_Administrativo
+GO
+
+GRANT SELECT ON Estado_Operacion_Crediticia TO Rol_Administrativo
+GO
+
+GRANT SELECT ON Estado_Movimiento TO Rol_Administrativo
+GO
+
+GRANT SELECT ON Tipo_Movimiento TO Rol_Administrativo
+GO
+
+GRANT SELECT ON Operacion_Crediticia TO Rol_Administrativo
+GO
+
+GRANT SELECT ON Movimiento TO Rol_Administrativo
+GO
+
+GRANT SELECT ON Garantia_Operacion_Crediticia TO Rol_Administrativo
+GO
+/*Inserts*/
+GRANT INSERT ON Tipo_Persona TO Rol_Administrativo
+GO
+
+GRANT INSERT ON Formacion_Educativa TO Rol_Administrativo
+GO
+
+GRANT INSERT ON Tipo_Mecanismo TO Rol_Administrativo
+GO
+
+GRANT INSERT ON Categoria TO Rol_Administrativo
+GO
+
+GRANT INSERT ON Tipo_Identificacion TO Rol_Administrativo
+GO
+
+GRANT INSERT ON Identificacion TO Rol_Administrativo
+GO
+
+GRANT INSERT ON Persona TO Rol_Administrativo
+GO
+
+GRANT INSERT ON Tipo_Estado_Persona TO Rol_Administrativo
+GO
+
+GRANT INSERT ON Persona_Tipo TO Rol_Administrativo
+GO
+
+GRANT INSERT ON Estado_Persona TO Rol_Administrativo 
+GO
+
+GRANT INSERT ON Mecanismo_Contacto TO Rol_Administrativo
+GO
+
+GRANT INSERT ON Moneda TO Rol_Administrativo
+GO
+
+GRANT INSERT ON Agencia TO Rol_Administrativo
+GO
+
+GRANT INSERT ON Tipo_Geografia TO Rol_Administrativo
+GO
+
+GRANT INSERT ON Tipo_Ingreso TO Rol_Administrativo
+GO
+
+GRANT INSERT ON Ingreso TO Rol_Administrativo
+GO
+
+GRANT INSERT ON Tipo_Direccion TO Rol_Administrativo
+GO
+
+GRANT INSERT ON Direccion TO Rol_Administrativo
+GO
+
+GRANT INSERT ON Geografia TO Rol_Administrativo
+GO
+
+GRANT INSERT ON Tipo_Relacion TO Rol_Administrativo
+GO
+
+GRANT INSERT ON Relacion TO Rol_Administrativo
+GO
+
+GRANT INSERT ON Sitio_Web TO Rol_Administrativo
+GO
+
+GRANT INSERT ON Naturaleza_Industria TO Rol_Administrativo 
+GO
+
+GRANT INSERT ON Industria_Persona TO Rol_Administrativo
+GO
+
+GRANT INSERT ON Garantia TO Rol_Administrativo
+GO
+
+GRANT INSERT ON Tipo_Operacion_Crediticia TO Rol_Administrativo
+GO
+
+GRANT INSERT ON Estado_Operacion_Crediticia TO Rol_Administrativo
+GO
+
+GRANT INSERT ON Estado_Movimiento TO Rol_Administrativo
+GO
+
+GRANT INSERT ON Tipo_Movimiento TO Rol_Administrativo
+GO
+
+GRANT INSERT ON Operacion_Crediticia TO Rol_Administrativo
+GO
+
+GRANT INSERT ON Movimiento TO Rol_Administrativo
+GO
+
+GRANT INSERT ON Garantia_Operacion_Crediticia TO Rol_Administrativo
+GO
