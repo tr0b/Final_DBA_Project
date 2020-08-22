888e62a3510e1c94037cb2d5589289fbbd2a1b33
tr0b <trobin3000600@gmail.com>
2020-08-21 19:30:37 -0600

    Final Project DBA -Fixed view person and income

--- view_creation.sql
+++ view_creation.sql
@@ -1,31 +1,30 @@
 CREATE VIEW v_Tipo_Persona WITH SCHEMABINDING AS
 
-SELECT ID, Nombre FROM Tipo_Persona
+SELECT ID, Nombre FROM dbo.Tipo_Persona
 GO
-
 CREATE VIEW v_Formacion_Educativa WITH SCHEMABINDING AS
 
-SELECT ID, Descripcion FROM Formacion_Educativa
+SELECT ID, Descripcion FROM dbo.Formacion_Educativa
 GO
 
 CREATE VIEW v_Tipo_Mecanismo WITH SCHEMABINDING AS
 
-SELECT ID, Descripcion FROM Tipo_Mecanismo
+SELECT ID, Descripcion FROM dbo.Tipo_Mecanismo
 GO
 
 CREATE VIEW v_Categoria WITH SCHEMABINDING AS
 
-SELECT ID, Nombre FROM Categoria
+SELECT ID, Nombre FROM dbo.Categoria
 GO
 
 CREATE VIEW v_Tipo_Identificacion WITH SCHEMABINDING AS
 
-SELECT ID, Descripcion FROM Tipo_Identificacion
+SELECT ID, Descripcion FROM dbo.Tipo_Identificacion
 GO
 
 CREATE VIEW v_Identificacion WITH SCHEMABINDING AS
 
-SELECT ID, Numero, FK_Tipo_Identificacion_ID FROM Identificacion
+SELECT ID, Numero, FK_Tipo_Identificacion_ID FROM dbo.Identificacion
 GO
 
 CREATE VIEW v_Persona WITH SCHEMABINDING AS
@@ -37,20 +36,20 @@ SELECT
 	,Segundo_Nombre  
 	,Primer_Apellido   
 	,Segundo_Apellido  
-	,Genero TINYINT 
+	,Genero 
 	,Fecha_Nac 
 	,Estado_Civil 
-	,Juridica BIT  
-	,FK_Formacion_Educativa_ID TINYINT 
+	,Juridica  
+	,FK_Formacion_Educativa_ID
 	,FK_Identificacion_ID 
 	,FK_Categoria_ID 
 
-FROM Persona
+FROM dbo.Persona
 GO
 
 CREATE VIEW v_Tipo_Estado_Persona WITH SCHEMABINDING AS
 
-SELECT ID, Nombre FROM Tipo_Estado_Persona
+SELECT ID, Nombre FROM dbo.Tipo_Estado_Persona
 GO
 
 CREATE VIEW v_Persona_Tipo WITH SCHEMABINDING AS
@@ -59,19 +58,19 @@ SELECT
 	ID 
 	,FK_Persona_ID
 	,FK_Tipo_ID
-FROM Persona_Tipo
+FROM dbo.Persona_Tipo
 GO
 
 CREATE VIEW v_Estado_Persona WITH SCHEMABINDING AS 
 
 SELECT 
 
-	,FechaInicio
+	FechaInicio
 	,FechaFin
 	,FK_Tipo_Estado_ID
 	,FK_Persona_ID
 
-FROM Estado_Persona
+FROM dbo.Estado_Persona
 GO
 
 CREATE VIEW v_Mecanismo_Contacto WITH SCHEMABINDING AS
@@ -82,7 +81,7 @@ SELECT
 	,Descripcion 
 	,FK_Tipo_Mecanismo_ID 
 	,FK_Persona_ID
-FROM Mecanismo_Contacto
+FROM dbo.Mecanismo_Contacto
 GO
 
 CREATE VIEW v_Moneda WITH SCHEMABINDING AS
@@ -92,7 +91,7 @@ SELECT
 	ID 
 	,Nombre 
 	,IsoAlpha3 
-FROM Moneda
+FROM dbo.Moneda
 GO
 
 CREATE VIEW v_Agencia WITH SCHEMABINDING AS
@@ -101,7 +100,7 @@ SELECT
 
 	ID 
 	,Descripcion
-FROM Agencia
+FROM dbo.Agencia
 GO
 
 CREATE VIEW v_Tipo_Geografia WITH SCHEMABINDING AS
@@ -111,7 +110,7 @@ SELECT
 	ID 
 	,Nombre 
 
-FROM Tipo_Geografia
+FROM dbo.Tipo_Geografia
 GO
 
 CREATE VIEW v_Tipo_Ingreso WITH SCHEMABINDING AS
@@ -121,19 +120,18 @@ SELECT
 	ID 
 	,Nombre 
 	,IsoAlpha3 
-FROM Tipo_Ingreso
+FROM dbo.Tipo_Ingreso
 GO
 
 CREATE VIEW v_Ingreso WITH SCHEMABINDING AS
 
+SELECT 
 	ID 
 	,Monto 
 	,Planilla 
 	,FK_Persona_ID 
 	,FK_Tipo_Ingreso_ID 
-SELECT 
-
-FROM Ingreso
+FROM dbo.Ingreso
 GO
 
 CREATE VIEW v_Tipo_Direccion WITH SCHEMABINDING AS
@@ -142,7 +140,7 @@ SELECT
 
 	ID 
 	,Nombre 
-FROM Tipo_Direccion
+FROM dbo.Tipo_Direccion
 GO
 
 CREATE VIEW v_Direccion WITH SCHEMABINDING AS
@@ -157,7 +155,7 @@ SELECT
 	,FK_Tipo_Direccion_ID 
 	,FK_Geografia_ID 
 	,FK_Persona_ID 
-FROM Direccion
+FROM dbo.Direccion
 GO
 
 CREATE VIEW v_Geografia WITH SCHEMABINDING AS
@@ -168,7 +166,7 @@ SELECT
 	,Nombre 
 	,FK_GeografiaID 
 	,FK_Tipo_Geografia_ID 
-FROM Geografia
+FROM dbo.Geografia
 GO
 
 CREATE VIEW v_Tipo_Relacion WITH SCHEMABINDING AS
@@ -177,7 +175,7 @@ SELECT
 
 	ID 
 	,Descripcion 
-FROM Tipo_Relacion
+FROM dbo.Tipo_Relacion
 GO
 
 CREATE VIEW v_Relacion WITH SCHEMABINDING AS
@@ -188,7 +186,7 @@ SELECT
 	,FK_Persona1_ID 
 	,FK_Persona2_ID 
 	,FK_Tipo_Relacion 
-FROM Relacion
+FROM dbo.Relacion
 GO
 
 CREATE VIEW v_Sitio_Web WITH SCHEMABINDING AS
@@ -199,7 +197,7 @@ SELECT
 	,Descripcion 
 	,FK_Persona_ID 
 	
-FROM Sitio_Web
+FROM dbo.Sitio_Web
 GO
 
 CREATE VIEW v_Naturaleza_Industria WITH SCHEMABINDING AS 
@@ -208,7 +206,7 @@ SELECT
 
 	ID 
 	,Nombre 
-FROM Naturaleza_Industria
+FROM dbo.Naturaleza_Industria
 GO
 
 CREATE VIEW v_Industria_Persona WITH SCHEMABINDING AS
@@ -218,7 +216,7 @@ SELECT
 	ID 
 	,FK_Persona_ID
 	,FK_Naturaleza_Industria_ID
-FROM Industria_Persona
+FROM dbo.Industria_Persona
 GO
 
 CREATE VIEW v_Garantia WITH SCHEMABINDING AS
@@ -227,7 +225,7 @@ SELECT
 
 	ID 
 	,Descripcion 
-FROM Garantia
+FROM dbo.Garantia
 GO
 
 CREATE VIEW v_Tipo_Operacion_Crediticia WITH SCHEMABINDING AS
@@ -236,7 +234,7 @@ SELECT
 
 	ID 
 	,Descripcion
-FROM Tipo_Operacion_Crediticia
+FROM dbo.Tipo_Operacion_Crediticia
 GO
 
 CREATE VIEW v_Estado_Operacion_Crediticia WITH SCHEMABINDING AS
@@ -245,7 +243,7 @@ SELECT
 
 	ID 
 	,Descripcion 
-FROM Estado_Operacion_Crediticia
+FROM dbo.Estado_Operacion_Crediticia
 GO
 
 CREATE VIEW v_Estado_Movimiento WITH SCHEMABINDING AS
@@ -255,7 +253,7 @@ SELECT
 	ID 
 	,Descripcion 
 
-FROM Estado_Movimiento
+FROM dbo.Estado_Movimiento
 GO
 
 CREATE VIEW v_Tipo_Movimiento WITH SCHEMABINDING AS
@@ -265,7 +263,7 @@ SELECT
 	ID 
 	,Descripcion 
 
-FROM Tipo_Movimiento
+FROM dbo.Tipo_Movimiento
 GO
 
 CREATE VIEW v_Operacion_Crediticia WITH SCHEMABINDING AS
@@ -301,7 +299,7 @@ SELECT
 	,FK_Estado_Operacion_Crediticia_ID
 	,FK_Tipo_Operacion_Crediticia_ID
 	,FK_Agencia_ID
-FROM Operacion_Crediticia
+FROM dbo.Operacion_Crediticia
 GO
 
 CREATE VIEW v_Movimiento WITH SCHEMABINDING AS
@@ -315,7 +313,7 @@ SELECT
 	,FK_Moneda_ID 
 	,FK_Tipo_Movimiento_ID 
 	,FK_Estado_Movimiento_ID 
-FROM Movimiento
+FROM dbo.Movimiento
 GO
 
 CREATE VIEW v_Garantia_Operacion_Crediticia WITH SCHEMABINDING AS
@@ -325,5 +323,5 @@ SELECT
 	ID 
 	,FK_Operacion_Crediticia_ID 
 	,FK_Garantia_ID 
-FROM Garantia_Operacion_Crediticia
+FROM dbo.Garantia_Operacion_Crediticia
 GO
